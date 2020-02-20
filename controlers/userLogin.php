f6e0ccd628245903ecc6b681196d904bb6603815
maincoke <maincoke@gmail.com>
2020-02-20 18:19:43 -0500

    Borrado de comentarios y lineas de prueba

--- controlers/userLogin.php
+++ controlers/userLogin.php
@@ -11,10 +11,8 @@ $loginChk = $user->checkLogin($nameuser, $passuser);
   if ($loginChk == -1) {
     header('Location: ../views/main.php');
   } else if ($loginChk == 1) {
-    //echo '<script>window.location: "../views/login.php?login=1"; $("#message-error").show().fadeOut(2500);</script>';
     header('Location: ../views/login.php?login=1');
   } else {
-    //echo '<script>window.location: "../views/login.php?login=0"; $("#message-error").show().fadeOut(2500);</script>';
     header('Location: ../views/login.php?login=0');
   }
 ?>
